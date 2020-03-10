173c2da988fdc1cfe73bb5379ca403149a48e385
[Pierrick] <pierrick.delrieu@efrei.net>
2020-03-11 00:09:39 +0100

    class Terrain et fichier image

---
+++ constante.py
@@ -0,0 +1,11 @@
+
+#constante
+largeur_terrain = 44 #tuiles
+hauteur_terrain = 22 #tuiles
+
+largeur_tuiles = 32 #pixes
+hauteur_tuiles = 32 #pixels
+
+taille_perso = (64,128)
+
+g = 9,81 #constante de gravitation
---
+++ main.py
@@ -0,0 +1,32 @@
+from Terrain import Terrain
+from Perso import Perso
+import pygame
+from constante import *
+import importlib #permet de personnalisé l'import d'une librairie
+
+theme = "minecraft"
+niveau = "1"
+tour_perso = 1
+tab_terrain = importlib.import_module("terrain."+theme+".terrain"+niveau)
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((largeur_terrain* largeur_tuiles, hauteur_terrain * hauteur_tuiles))
+
+terrain = Terrain (tab_terrain.modele_terrain, theme, fenetre)
+terrain.rendu_terrain()
+
+
+
+
+while (True):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            break
+        
+    pygame.display.flip()
+    
+    
+    
+    
+    
