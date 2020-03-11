7d2c2a8dbb1e2bb0159e03c5094485fae919858f
[Pierrick] <pierrick.delrieu@efrei.net>
2020-03-11 10:37:24 +0100

    mise au propre

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from constante import *
 import importlib #permet de personnalisé l'import d'une librairie
 
 theme = "minecraft"
-niveau = "1"
+niveau = "1" #le niveau correspond au tableau 2D de la map
 tour_perso = 1
 tab_terrain = importlib.import_module("terrain."+theme+".terrain"+niveau)
 
-pygame.init()
+pygame.init() #initialise pygame
 
-fenetre = pygame.display.set_mode((largeur_terrain* largeur_tuiles, hauteur_terrain * hauteur_tuiles))
+fenetre = pygame.display.set_mode((largeur_terrain* largeur_tuiles, hauteur_terrain * hauteur_tuiles)) #initialise la fenetre
 
-terrain = Terrain (tab_terrain.modele_terrain, theme, fenetre)
-terrain.rendu_terrain()
+terrain = Terrain (tab_terrain.modele_terrain, theme, fenetre) #initialise la class Terrain
+terrain.rendu_terrain() #affichage du terrain
 
 
 
